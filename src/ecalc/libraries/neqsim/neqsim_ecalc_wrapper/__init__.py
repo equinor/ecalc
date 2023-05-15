141228046b3ff2cc2b10941422696a18e8d90ad6
Jørgen <jenge@equinor.com>
2023-05-15 07:52:59 +0200

    refactor: remove unused code

--- src/ecalc/libraries/neqsim/neqsim_ecalc_wrapper/__init__.py
+++ src/ecalc/libraries/neqsim/neqsim_ecalc_wrapper/__init__.py
@@ -3,15 +3,16 @@ from neqsim_ecalc_wrapper.java_service import start_server
 java_gateway = start_server()
 neqsim = java_gateway.jvm.neqsim
 
-from neqsim_ecalc_wrapper.thermo import NeqsimEoSModelType, NeqsimFluid  # noqa
+from neqsim_ecalc_wrapper.thermo import NeqsimEoSModelType, NeqsimFluid
 
 
-def methods(checkClass):  # noqa
-    methods = checkClass.getClass().getMethods()
-    for method in methods:
-        print(method.getName())
-
-
-def setDatabase(connectionString):  # noqa
-    neqsim.util.database.NeqSimDataBase.setConnectionString(connectionString)
-    neqsim.util.database.NeqSimDataBase.setCreateTemporaryTables(True)
+def methods(check_class):
+    """
+    Print list of available methods for a java class
+    """
+    try:
+        list_of_methods = check_class.getClass().getMethods()
+        for method in list_of_methods:
+            print(method.getName())
+    except AttributeError:
+        print(f"Unknown attribute for {check_class}, the class might not be a java class")
