e708843784df8e9d39fe8ae996b2d9ed4c28f531
Markus Rye Foss <markus.foss@bouvet.no>
2023-06-22 14:41:30 +0200

    fix: parse scientific notation numbers in expression
    The expression lexer was missing parsing for scientific notation
    (e.g. 1.23e-05 or 3.4e7) for numbers part of an expressions with
    multiple tokens.
    
    Ex. expression '2 {*} 3.4e-05' would be parsed as tokens
    [2, {*}, 3.4, e-05], which would give issues as there are no
    variables named e-05.

--- src/ecalc/libraries/libecalc/common/tests/expression/test_expression_evaluator.py
+++ src/ecalc/libraries/libecalc/common/tests/expression/test_expression_evaluator.py
@@ -94,6 +94,55 @@ def test_lexer():
     assert token_values == compare
 
 
+# disable formatting to prevent black from removing '+' from numbers with scientific notation
+# fmt: off
+scientific_expressions = [
+    ("1.1e1", [1.1e1], [TokenTag.numeric]),
+    ("1.1e111", [1.1e111], [TokenTag.numeric]),
+    ("1.1e+1", [1.1e+1], [TokenTag.numeric]),
+    ("1.1e-1", [1.1e-1], [TokenTag.numeric]),
+    ("1.1e-000001", [1.1e-000001], [TokenTag.numeric]),
+    ("1.1e+111", [1.1e+111], [TokenTag.numeric]),
+    ("1.1e-111", [1.1e-111], [TokenTag.numeric]),
+    ("1.111e1", [1.111e1], [TokenTag.numeric]),
+    ("1.111e+1", [1.111e+1], [TokenTag.numeric]),
+    ("1.111e-1", [1.111e-1], [TokenTag.numeric]),
+    ("1.111e-111", [1.111e-111], [TokenTag.numeric]),
+    ("1e1", [1e1], [TokenTag.numeric]),
+    ("1e+1", [1e+1], [TokenTag.numeric]),
+    ("1e-1", [1e-1], [TokenTag.numeric]),
+    ("1e-111", [1e-111], [TokenTag.numeric]),
+    (
+        "2.4e6 {/} 100 {*} 1.35e-05{+}  2e14 {-} 6.543e+11 {^} VARIABLE;VAL1",
+        [2.4e6, "{/}", 100.0, "{*}", 1.35e-05, "{+}", 2e14, "{-}", 6.543e+11, "{^}", "VARIABLE;VAL1"],
+        [
+            TokenTag.numeric,
+            TokenTag.operator,
+            TokenTag.numeric,
+            TokenTag.operator,
+            TokenTag.numeric,
+            TokenTag.operator,
+            TokenTag.numeric,
+            TokenTag.operator,
+            TokenTag.numeric,
+            TokenTag.operator,
+            TokenTag.reference,
+        ],
+    ),
+]
+# fmt: on
+
+
+@pytest.mark.parametrize(
+    "expression, expected, expected_token_tags",
+    scientific_expressions,
+)
+def test_lexer_scientific(expression, expected, expected_token_tags):
+    tokens = lexer(expression)
+    assert [token.value for token in tokens] == expected
+    assert [token.tag for token in tokens] == expected_token_tags
+
+
 def test_Powers(caplog):
     caplog.set_level("CRITICAL")
     with pytest.raises(Exception):
