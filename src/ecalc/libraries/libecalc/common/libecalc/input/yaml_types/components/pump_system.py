0fab7068addeba43f1e2554af9a9292d8952cd49
Jostein Solaas <josteinsolaas@gmail.com>
2023-09-11 15:14:32 +0200

    fix: generate system v2 schema

--- src/ecalc/libraries/libecalc/common/libecalc/input/yaml_types/components/pump_system.py
+++ src/ecalc/libraries/libecalc/common/libecalc/input/yaml_types/components/pump_system.py
@@ -65,26 +65,26 @@ class OperationalSettings(ConsumerSystemOperationalConditionBase):
     crossover: Optional[List[int]] = Field(
         None,
         title="Crossover",
-        description="""
-CROSSOVER specifies if rates are to be crossed over to another consumer if rate capacity is exceeded. If the energy \n
-consumption calculation is not successful for a consumer, and the consumer has a valid cross-over defined, the \n
-consumer will be allocated its maximum rate and the exceeding rate will be added to the cross-over consumer. \n
-To avoid loops, a consumer can only be either receiving or giving away rate. For a cross-over to be valid, the \n
-discharge pressure at the consumer "receiving" overshooting rate must be higher than or equal to the discharge \n
-pressure of the "sending" consumer. This is because it is possible to choke pressure down to meet the outlet pressure \n
-in a flow line with lower pressure, but not possible to "pressure up" in the crossover flow line. \n
-Some examples show how the crossover logic works:\n\n
-Crossover is given as and list of integer values for the first position is the first consumer, second position is \n
-the second consumer, etc. The number specifies which consumer to send cross-over flow to, and 0 signifies no \n
-cross-over possible. Note that we use 1-index here.\n\n
-Example 1:\n
-Two consumers where there is a cross-over such that if the rate for the first consumer exceeds its capacity, \n
-the excess rate will be processed by the second consumer. The second consumer can not cross-over to anyone. \n\n
-CROSSOVER: [2, 0]\n\n
-Example 2:\n
-The first and second consumers may both send exceeding rate to the third consumer if their capacity is exceeded.\n\n
-CROSSOVER: [3,3,0]
-""",
+        description=(
+            "CROSSOVER specifies if rates are to be crossed over to another consumer if rate capacity is exceeded. If the energy"
+            " consumption calculation is not successful for a consumer, and the consumer has a valid cross-over defined, the"
+            " consumer will be allocated its maximum rate and the exceeding rate will be added to the cross-over consumer.\n"
+            "To avoid loops, a consumer can only be either receiving or giving away rate. For a cross-over to be valid, the"
+            ' discharge pressure at the consumer "receiving" overshooting rate must be higher than or equal to the discharge'
+            ' pressure of the "sending" consumer. This is because it is possible to choke pressure down to meet the outlet pressure'
+            ' in a flow line with lower pressure, but not possible to "pressure up" in the crossover flow line.\n'
+            "Some examples show how the crossover logic works:\n"
+            "Crossover is given as and list of integer values for the first position is the first consumer, second position is"
+            " the second consumer, etc. The number specifies which consumer to send cross-over flow to, and 0 signifies no"
+            " cross-over possible. Note that we use 1-index here.\n"
+            "Example 1:\n"
+            "Two consumers where there is a cross-over such that if the rate for the first consumer exceeds its capacity,"
+            " the excess rate will be processed by the second consumer. The second consumer can not cross-over to anyone.\n"
+            "CROSSOVER: [2, 0]\n"
+            "Example 2:\n"
+            "The first and second consumers may both send exceeding rate to the third consumer if their capacity is exceeded.\n"
+            "CROSSOVER: [3,3,0]"
+        ),
     )
 
     @validator("rate_fractions", always=True)
