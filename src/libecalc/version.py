5656021699a0b28bd8b09249443270bcccbb37ea
Thomas Johansen <tj098895@equinor.com>
2023-09-25 22:03:09 +0200

    chore: version must be updated in version.py
    Incorrect version.py file was referred to in
    release-please, and hence the version within libecalc
    was not updated. showing v8.3.0 even though it was
    v8.4.0.

--- src/libecalc/version.py
+++ src/libecalc/version.py
@@ -1,13 +1,13 @@
 from libecalc.common.version import Version
 
-# DO NOT EDIT - replaced in CI
+# DO NOT EDIT - replaced in CI with release please
 __version__ = "8.3.0"  # x-release-please-version
 # END DO NOT EDIT
 
 
 def current_version() -> Version:
     """Get the current version of eCalc. This is set and
-    built in in CICD pipeline. Locally it will always be 0.0.0
+    built in the CICD pipeline.
     :return:
     """
     return Version.from_string(__version__)
