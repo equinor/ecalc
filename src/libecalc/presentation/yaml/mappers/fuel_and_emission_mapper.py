d3d88b641a878db24f6bf54b315903ce4c36873f
Frode Helgetun Krogh <70878501+frodehk@users.noreply.github.com>
2023-11-16 13:36:18 +0100

    refactor: add logger warning for deprecated variables

--- src/libecalc/presentation/yaml/mappers/fuel_and_emission_mapper.py
+++ src/libecalc/presentation/yaml/mappers/fuel_and_emission_mapper.py
@@ -1,5 +1,6 @@
 from typing import Dict
 
+from ecalc_cli.logger import logger
 from pydantic import ValidationError
 
 from libecalc import dto
@@ -10,6 +11,8 @@ from libecalc.presentation.yaml.yaml_keywords import EcalcYamlKeywords
 class EmissionMapper:
     @staticmethod
     def from_yaml_to_dto(data: Dict) -> dto.Emission:
+        if data.get("TAX") or data.get("QUOTA"):
+            logger.warning("Emission tax and quota are deprecated. It will have no effect.")
         return dto.Emission(
             name=data.get(EcalcYamlKeywords.name),
             factor=data.get(EcalcYamlKeywords.emission_factor),
@@ -20,6 +23,9 @@ class FuelMapper:
     @staticmethod
     def from_yaml_to_dto(fuel: Dict) -> dto.types.FuelType:
         try:
+            if fuel.get("PRICE"):
+                logger.warning("Fuel price is deprecated. It will have no effect.")
+
             return dto.types.FuelType(
                 name=fuel.get(EcalcYamlKeywords.name),
                 user_defined_category=fuel.get(EcalcYamlKeywords.user_defined_tag),
