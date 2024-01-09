59be31741903845388068cfbadc23b39cb48b35b
Jostein Solaas <josteinsolaas@gmail.com>
2024-01-09 11:47:12 +0100

    refactor: rename file
    ecalc.py is actually energy calculator

--- src/tests/libecalc/integration/test_multiple_installations.py
+++ src/tests/libecalc/integration/test_multiple_installations.py
@@ -4,7 +4,7 @@ import pytest
 from libecalc import dto
 from libecalc.common.units import Unit
 from libecalc.common.utils.rates import RateType, TimeSeriesRate
-from libecalc.core.ecalc import EnergyCalculator
+from libecalc.core.energy_calculator import EnergyCalculator
 from libecalc.core.graph_result import GraphResult
 
 
