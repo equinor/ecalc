3622927e0defc0b33b0f15a55d09be284d767363
Jostein Solaas <josteinsolaas@gmail.com>
2024-02-15 21:21:51 +0100

    fix: issue with TimeSeriesBoolean becoming float
    Added check so we don't format booleans to precision, thereby avoiding
    converting booleans to floats.
    
    This also removes the pydantic serializer warnings.

---
+++ src/tests/libecalc/integration/test_minimal_is_valid.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+import pytest
+from libecalc.application.energy_calculator import EnergyCalculator
+from libecalc.application.graph_result import GraphResult
+from libecalc.dto import VariablesMap
+from libecalc.dto.result import EcalcModelResult
+
+
+@pytest.fixture
+def minimal_asset_result(minimal_model_dto_factory):
+    minimal_dto = minimal_model_dto_factory()
+    graph = minimal_dto.get_graph()
+    variables = VariablesMap(
+        time_vector=[datetime(2020, 1, 1), datetime(2022, 1, 1)],
+        variables={},
+    )
+    energy_calculator = EnergyCalculator(graph=graph)
+    consumer_results = energy_calculator.evaluate_energy_usage(variables)
+    emission_results = energy_calculator.evaluate_emissions(
+        variables_map=variables,
+        consumer_results=consumer_results,
+    )
+    return GraphResult(
+        graph=graph,
+        consumer_results=consumer_results,
+        variables_map=variables,
+        emission_results=emission_results,
+    ).get_asset_result()
+
+
+def test_is_valid_is_boolean(minimal_asset_result: EcalcModelResult):
+    """
+    We had a bug where all TimeSeriesBoolean values became floats because of rounding,
+    this makes sure that does not happen again.
+    """
+    assert all(type(value) is bool for value in minimal_asset_result.component_result.is_valid.values)
