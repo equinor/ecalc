fix: numerical issues due to rounding to a number of significant digit in chart curves

src/libecalc/core/models/compressor/train/chart/chart_creator.py
@@ -16,7 +16,6 @@ from libecalc.core.models.compressor.train.chart.variable_speed_compressor_chart
 )
 from libecalc.core.models.compressor.train.utils.common import (
     PRESSURE_CALCULATION_TOLERANCE,
-    RATE_CALCULATION_TOLERANCE,
 )
 from libecalc.core.models.compressor.train.utils.numeric_methods import (
     maximize_x_given_boolean_condition_function,
@@ -76,10 +75,10 @@ class CompressorChartCreator:
             maximum_head_on_maximum_speed_curve_unified - minimum_head_on_maximum_speed_curve_unified
         )
 
-        # Maximum values from input - scaling them slightly up to avoid having points exactly at the ends of the
+        # Maximum values from input - scaling head values slightly to avoid having points exactly at the ends of the
         # maximum speed curve, which potentially leads to numerical issues with the current implementation (rounding
         # chart curves to a certain number of significant digits since they are also exported in results)
-        maximum_actual_rate = np.max(actual_volume_rates_m3_per_hour) * (1 + RATE_CALCULATION_TOLERANCE)
+        maximum_actual_rate = np.max(actual_volume_rates_m3_per_hour)
         maximum_head_value = np.max(heads_joule_per_kg) * (1 + PRESSURE_CALCULATION_TOLERANCE)
 
         # Values to scale input with