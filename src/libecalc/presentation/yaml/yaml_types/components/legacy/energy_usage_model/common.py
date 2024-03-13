2af9d1332864b3e3bf17f84bd980b8bd436e7c7a
Jostein Solaas <33114722+jsolaas@users.noreply.github.com>
2024-03-13 11:38:41 +0100

    feat: validate expression reference tokens (#404)
    If a context with expression references are given, the
    YamlExpressionType will now check that the references in the expression
    are valid.

--- src/libecalc/presentation/yaml/yaml_types/components/legacy/energy_usage_model/common.py
+++ src/libecalc/presentation/yaml/yaml_types/components/legacy/energy_usage_model/common.py
@@ -2,22 +2,24 @@ from typing import List
 
 from pydantic import Field
 
-from libecalc.expression.expression import ExpressionType
 from libecalc.presentation.yaml.yaml_types import YamlBase
+from libecalc.presentation.yaml.yaml_types.components.yaml_expression_type import (
+    YamlExpressionType,
+)
 
 
 class EnergyUsageModelCommon(YamlBase):
-    condition: ExpressionType = Field(
+    condition: YamlExpressionType = Field(
         None,
         title="CONDITION",
         description="Logical condition for the consumer to be used.\n\n$ECALC_DOCS_KEYWORDS_URL/CONDITION",
     )
-    conditions: List[ExpressionType] = Field(
+    conditions: List[YamlExpressionType] = Field(
         None,
         title="CONDITIONS",
         description="Logical conditions for the consumer to be used.\n\n$ECALC_DOCS_KEYWORDS_URL/CONDITION",
     )
-    power_loss_factor: ExpressionType = Field(
+    power_loss_factor: YamlExpressionType = Field(
         None,
         title="POWERLOSSFACTOR",
         description="A factor that may be added to account for power line losses.\n\n$ECALC_DOCS_KEYWORDS_URL/POWERLOSSFACTOR",
---
+++ src/libecalc/presentation/yaml/yaml_types/components/yaml_expression_type.py
@@ -0,0 +1,49 @@
+from pydantic import AfterValidator
+from pydantic_core import PydanticCustomError
+from pydantic_core.core_schema import ValidationInfo
+from typing_extensions import Annotated
+
+from libecalc.expression.expression import (
+    Expression,
+    ExpressionType,
+    InvalidExpressionError,
+)
+from libecalc.expression.expression_evaluator import TokenTag
+from libecalc.presentation.yaml.yaml_validation_context import (
+    YamlModelValidationContextNames,
+)
+
+
+def validate_expression(value: ExpressionType, info: ValidationInfo) -> ExpressionType:
+    if info.context is None or YamlModelValidationContextNames.expression_tokens not in info.context:
+        # Skip if context not provided
+        return value
+
+    available_expression_tokens = info.context[YamlModelValidationContextNames.expression_tokens]
+    unknown_tokens = []
+    try:
+        tokens = Expression.validate(expression=value)
+        for token in tokens:
+            if token.tag == TokenTag.reference and token.value not in available_expression_tokens:
+                unknown_tokens.append(token.value)
+
+        if len(unknown_tokens) > 0:
+            expression_references_string = ", ".join(unknown_tokens)
+            raise PydanticCustomError(
+                "expression_reference_not_found",
+                "Expression reference(s) {expression_references_string} does not exist.",
+                {
+                    "expression_references_string": expression_references_string,
+                    "expression_references": unknown_tokens,
+                },
+            )
+    except InvalidExpressionError as e:
+        raise ValueError(str(e)) from e
+
+    return value
+
+
+YamlExpressionType = Annotated[
+    ExpressionType,
+    AfterValidator(validate_expression),
+]
