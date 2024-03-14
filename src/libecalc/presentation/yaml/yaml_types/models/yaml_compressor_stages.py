0f1e6f8af15208ce613f9d39fe1de4f8edd9e72f
Jostein Solaas <33114722+jsolaas@users.noreply.github.com>
2024-03-14 09:34:22 +0100

    fix: add control margin to compressor stage validation (#405)
    Refs: ECALC-883

--- src/libecalc/presentation/yaml/yaml_types/models/yaml_compressor_stages.py
+++ src/libecalc/presentation/yaml/yaml_types/models/yaml_compressor_stages.py
@@ -1,8 +1,7 @@
 import enum
-from typing import List, Literal, Optional, Union
+from typing import List, Optional, Union
 
 from pydantic import Field
-from typing_extensions import Annotated
 
 from libecalc.presentation.yaml.yaml_types import YamlBase
 from libecalc.presentation.yaml.yaml_types.models.yaml_enums import YamlPressureControl
@@ -40,16 +39,11 @@ class YamlCompressorStageBase(YamlBase):
 
 
 class YamlSingleSpeedCompressorStage(YamlCompressorStageBase):
-    pressure_drop_ahead_of_stage: Optional[
-        Annotated[
-            float,
-            Field(
-                ...,
-                description="Pressure drop before compression stage [in bar]",
-                title="PRESSURE_DROP_AHEAD_OF_STAGE",
-            ),
-        ]
-    ] = None
+    pressure_drop_ahead_of_stage: Optional[float] = Field(
+        None,
+        description="Pressure drop before compression stage [in bar]",
+        title="PRESSURE_DROP_AHEAD_OF_STAGE",
+    )
     compressor_chart: str = Field(
         ...,
         description="Reference to compressor chart model for stage, must be defined in MODELS or FACILITY_INPUTS",
@@ -58,91 +52,77 @@ class YamlSingleSpeedCompressorStage(YamlCompressorStageBase):
 
 
 class YamlVariableSpeedCompressorStage(YamlCompressorStageBase):
-    pressure_drop_ahead_of_stage: float = Field(
+    pressure_drop_ahead_of_stage: Optional[float] = Field(
         None,
         description="Pressure drop before compression stage [in bar]",
         title="PRESSURE_DROP_AHEAD_OF_STAGE",
     )
     compressor_chart: str
-    control_margin: float = Field(
-        None,
+    control_margin: Optional[float] = Field(
+        0.0,
+        description="Surge control margin, see documentation for more details.",
+        title="CONTROL_MARGIN",
     )
-    control_margin_unit: Literal["PERCENTAGE", "FRACTION"] = Field(None)
+    control_margin_unit: Optional[YamlControlMarginUnits] = Field(
+        YamlControlMarginUnits.PERCENTAGE,
+        description="The unit of the surge control margin.",
+        title="CONTROL_MARGIN_UNIT",
+    )
 
 
 class YamlCompressorStage(YamlCompressorStageBase):
-    pressure_drop_ahead_of_stage: Optional[
-        Annotated[
-            float,
-            Field(
-                ...,
-                description="Pressure drop before compression stage [in bar]",
-                title="PRESSURE_DROP_AHEAD_OF_STAGE",
-            ),
-        ]
-    ] = None
+    pressure_drop_ahead_of_stage: Optional[float] = Field(
+        None,
+        description="Pressure drop before compression stage [in bar]",
+        title="PRESSURE_DROP_AHEAD_OF_STAGE",
+    )
+    control_margin: Optional[float] = Field(
+        0.0,
+        description="Surge control margin, see documentation for more details.",
+        title="CONTROL_MARGIN",
+    )
+    control_margin_unit: Optional[YamlControlMarginUnits] = Field(
+        YamlControlMarginUnits.PERCENTAGE,
+        description="The unit of the surge control margin.",
+        title="CONTROL_MARGIN_UNIT",
+    )
 
 
 class YamlCompressorStageMultipleStreams(YamlCompressorStageBase):
-    control_margin: Optional[
-        Annotated[
-            float,
-            Field(
-                0.0,
-                description="Surge control margin, see documentation for more details.",
-                title="CONTROL_MARGIN",
-            ),
-        ]
-    ] = None
-    control_margin_unit: Optional[
-        Annotated[
-            YamlControlMarginUnits,
-            Field(
-                YamlControlMarginUnits.PERCENTAGE,
-                description="The unit of the surge control margin.",
-                title="CONTROL_MARGIN_UNIT",
-            ),
-        ]
-    ] = None
+    control_margin: Optional[float] = Field(
+        0.0,
+        description="Surge control margin, see documentation for more details.",
+        title="CONTROL_MARGIN",
+    )
+    control_margin_unit: Optional[YamlControlMarginUnits] = Field(
+        YamlControlMarginUnits.PERCENTAGE,
+        description="The unit of the surge control margin.",
+        title="CONTROL_MARGIN_UNIT",
+    )
     stream: Union[str, List[str]] = Field(
         None,
         description="Reference to stream from STREAMS.",
         title="STREAM",
     )
-    pressure_drop_ahead_of_stage: Optional[
-        Annotated[
-            float,
-            Field(
-                ...,
-                description="Pressure drop before compression stage [in bar]",
-                title="PRESSURE_DROP_AHEAD_OF_STAGE",
-            ),
-        ]
-    ] = None
-    interstage_control_pressure: Optional[
-        Annotated[
-            YamlInterstageControlPressure,
-            Field(
-                ...,
-                description="Pressure control. Can only be specified for one (only one) of the stages 2, ..., N.",
-                title="INTERSTAGE_CONTROL_PRESSURE",
-            ),
-        ]
-    ] = None
+    pressure_drop_ahead_of_stage: Optional[float] = Field(
+        None,
+        description="Pressure drop before compression stage [in bar]",
+        title="PRESSURE_DROP_AHEAD_OF_STAGE",
+    )
+    interstage_control_pressure: Optional[YamlInterstageControlPressure] = Field(
+        None,
+        description="Pressure control. Can only be specified for one (only one) of the stages 2, ..., N.",
+        title="INTERSTAGE_CONTROL_PRESSURE",
+    )
 
 
 class YamlUnknownCompressorStages(YamlCompressorStageBase):
-    maximum_pressure_ratio_per_stage: Optional[
-        Annotated[
-            float,
-            Field(
-                ...,
-                description="Maximum pressure ratio per stage. Number of compressors will be large enough to ensure no "
-                "pressure ratios are above a given maximum pressure ratio per stage, but not larger",
-                title="MAXIMUM_PRESSURE_RATIO_PER_STAGE",
-            ),
-        ]
-    ] = None
+    maximum_pressure_ratio_per_stage: Optional[float] = Field(
+        None,
+        description="Maximum pressure ratio per stage. Number of compressors will be large enough to ensure no "
+        "pressure ratios are above a given maximum pressure ratio per stage, but not larger",
+        title="MAXIMUM_PRESSURE_RATIO_PER_STAGE",
+    )
 
 
 class YamlCompressorStages(YamlBase):
