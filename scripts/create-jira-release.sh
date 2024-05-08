b17e67c479fc46b5c24c25744917dfd5e7320acf
Markus Rye Foss <markus.foss@bouvet.no>
2024-05-08 11:27:27 +0200

    chore: update jira automation script

--- scripts/create-jira-release.sh
+++ scripts/create-jira-release.sh
@@ -6,7 +6,7 @@ release_version=$1
 webhook_url=$2
 
 # Create array of sorted tags
-mapfile -t tags < <(git tag | sort -Vr)
+mapfile -t tags < <(git tag --list 'v*.*.*' | sort -Vr)
 
 echo "Found tags:"
 echo "${tags[@]}"
@@ -25,7 +25,7 @@ mapfile -t issues < <(git log "$previous_tag".."$release_version" | grep -oP 'EC
 issues_json=$(jq --compact-output --null-input '$ARGS.positional' --args -- "${issues[@]}")
 
 # Create webhook body
-body=$(jq --arg libecalcVersion "$release_version" --argjson issues_json "$issues_json" -Rn '{"issues": $issues_json|unique, "libecalcVersion": $libecalcVersion}')
+body=$(jq --arg version "$release_version" --argjson issues_json "$issues_json" -Rn '{"issues": $issues_json|unique, "version": $version}')
 
 echo "$body"
 
