d19b75e2e929b4984b17540d0bc164a1d7222799
Jostein Solaas <josas@equinor.com>
2024-06-03 14:10:45 +0200

    chore: fix katex equations

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -128,10 +128,10 @@ const config: Config = {
     } satisfies Preset.ThemeConfig,
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.js',
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.css',
       type: 'text/css',
       integrity:
-        'sha384-hIoBPJpTUs74ddyc4bFZSM1TVlQDA60VBbJS0oA934VSz82sBx1X7kSx2ATBDIyd',
+        'sha384-wcIxkf4k558AjM3Yz3BBFQUbk/zgIYC2R0QpeeYb+TwlBVMrlgLqwRjRtGZiK7ww',
       crossorigin: 'anonymous',
     },
   ],
