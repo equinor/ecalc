f0c317a39d27066c6aafc003c673ab0a78bd8b73
Jostein Solaas <josas@equinor.com>
2024-06-07 11:16:55 +0200

    fix: implement YamlModel specific errors
    YamlError gives additional info about the error in yaml, in addition to
    ensuring a consistent interface across different yaml readers.

---
+++ src/libecalc/presentation/yaml/yaml_models/exceptions.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from libecalc.presentation.yaml.file_context import FileContext
+
+
+class YamlError(Exception):
+    def __init__(self, problem: str, file_context: Optional[FileContext] = None):
+        self.problem = problem
+        self.file_context = file_context
+        message = f"{problem}"
+        if file_context is not None:
+            message += str(file_context)
+
+        super().__init__(message)
+
+
+class DuplicateKeyError(YamlError):
+    def __init__(self, key: str, file_context: FileContext):
+        self.key = key
+        super().__init__(f"Duplicate key {key!r} is found", file_context)
