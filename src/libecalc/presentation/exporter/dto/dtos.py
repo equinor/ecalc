d1a9d0722e520479325512dd3b07f2cb9c0901d2
Jostein Solaas <josas@equinor.com>
2024-06-11 10:09:52 +0200

    refactor: separate formatter from exporter
    Create separate types for Formatter

--- src/libecalc/presentation/exporter/dto/dtos.py
+++ src/libecalc/presentation/exporter/dto/dtos.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Dict, List, Set, Type, Union
+from typing import Dict, Iterator, List, Set, Tuple, Type, Union
 
 import pandas as pd
 from pydantic import Field, model_validator
@@ -16,14 +16,24 @@ from libecalc.common.math.math_utils import MathUtil
 from libecalc.common.time_utils import Frequency
 from libecalc.common.units import Unit
 from libecalc.dto.result.base import EcalcResultBaseModel
+from libecalc.presentation.exporter.formatters.exceptions import ColumnNotFound
+from libecalc.presentation.exporter.formatters.formatter import (
+    ColumnIndex,
+    Formattable,
+    FormattableGroup,
+    RowIndex,
+)
 
 
 @dataclass
 class DataSeries:
     name: str
     title: str
-
     values: List[Union[str, float]]
+
+    @property
+    def id(self) -> str:
+        return self.name
 
 
 @dataclass
@@ -36,10 +46,13 @@ class QueryResult:
 
     name: str
     title: str
-
     unit: Unit  # Needed! in order to know how to handler further....parse
-
     values: Dict[datetime, float]
+
+    @property
+    def id(self) -> str:
+        return self.name
+
     # TODO: split? we will at this level always have one time vector..?!
 
 
@@ -50,10 +63,61 @@ class GroupedQueryResult:
 
 
 @dataclass
-class FilteredResult:
+class FormattableGroupedQuery(Formattable):
+    data_series: List[DataSeries]
+    query_results: List[QueryResult]
+    time_vector: List[datetime]
+
+    @property
+    def row_ids(self) -> List[RowIndex]:
+        return self.time_vector
+
+    @property
+    def column_ids(self) -> List[ColumnIndex]:
+        column_ids = []
+        for data_serie in self.data_series:
+            column_ids.append(data_serie.id)
+        for query_result in self.query_results:
+            column_ids.append(query_result.id)
+        return column_ids
+
+    def get_column(self, column_id: ColumnIndex) -> Union[DataSeries, QueryResult]:
+        try:
+            return next(data_serie for data_serie in self.data_series if data_serie.id == column_id)
+        except StopIteration:
+            try:
+                return next(query_result for query_result in self.query_results if query_result.id == column_id)
+            except StopIteration as e:
+                raise ColumnNotFound(column_id) from e
+
+    def get_value(self, row_id: RowIndex, column_id: ColumnIndex) -> Union[int, float]:
+        column = self.get_column(column_id)
+        if isinstance(column, DataSeries):
+            index = self.row_ids.index(row_id)
+            return column.values[index]
+        else:
+            return column.values[row_id]
+
+
+@dataclass
+class FilteredResult(FormattableGroup):
     data_series: List[DataSeries]
     query_results: List[GroupedQueryResult]
     time_vector: List[datetime]
+
+    @property
+    def groups(self) -> Iterator[Tuple[str, Formattable]]:
+        return (
+            (
+                group.group_name,
+                FormattableGroupedQuery(
+                    data_series=self.data_series,
+                    time_vector=self.time_vector,
+                    query_results=group.query_results,
+                ),
+            )
+            for group in self.query_results
+        )
 
 
 class TimeSteps(EcalcResultBaseModel):
---
+++ src/tests/libecalc/output/results/formatters/test_csv_formatter.py
@@ -0,0 +1,77 @@
+from dataclasses import dataclass
+from typing import List, Union
+
+from libecalc.common.units import Unit
+from libecalc.domain.tabular.tabular import ColumnIndex
+from libecalc.presentation.exporter.formatters.formatter import (
+    CSVFormatter,
+    Formattable,
+)
+
+
+@dataclass
+class Column:
+    id: str
+    title: str
+    unit: Unit
+
+
+@dataclass
+class Data(Formattable):
+    rows: List[str]
+    columns: List[str]
+    value: int
+
+    def get_column(self, column_id: ColumnIndex) -> Column:
+        return Column(
+            id=column_id,
+            title=column_id.upper(),
+            unit=Unit.TONS,
+        )
+
+    @property
+    def row_ids(self) -> List[str]:
+        return self.rows
+
+    @property
+    def column_ids(self) -> List[str]:
+        return self.columns
+
+    def get_value(self, row_id: str, column_id: str) -> Union[int, float]:
+        return self.value
+
+
+class TestCSVFormatter:
+    def test_format_comma(self):
+        formatter = CSVFormatter()
+        rows = formatter.format(
+            Data(
+                rows=["row1", "row2"],
+                columns=["col1", "col2"],
+                value=5,
+            )
+        )
+
+        assert rows == [
+            "col1,col2",
+            "#COL1[t],COL2[t]",
+            "5,5",
+            "5,5",
+        ]
+
+    def test_format_tabs(self):
+        formatter = CSVFormatter(separation_character="\t")
+        rows = formatter.format(
+            Data(
+                rows=["row1", "row2"],
+                columns=["col1", "col2"],
+                value=5,
+            )
+        )
+
+        assert rows == [
+            "col1\tcol2",
+            "#COL1[t]\tCOL2[t]",
+            "5\t5",
+            "5\t5",
+        ]
