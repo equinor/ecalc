8527808a1030484cfff1f23d3ca975aea9f35425
Jostein Solaas <josas@equinor.com>
2024-06-13 10:17:36 +0200

    refactor: remove unnecessary id for column

--- src/libecalc/presentation/exporter/formatters/exceptions.py
+++ src/libecalc/presentation/exporter/formatters/exceptions.py
@@ -2,3 +2,9 @@ class ColumnNotFound(Exception):
     def __init__(self, column_id):
         self.column_id = column_id
         super().__init__(f"Column with id '{column_id}' not found")
+
+
+class RowNotFound(Exception):
+    def __init__(self, row_id):
+        self.row_id = row_id
+        super().__init__(f"Row with id '{row_id}' not found")
