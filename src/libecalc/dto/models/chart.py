d962075b62798f01f9639c8a3b196a9233c5149e
Frode Helgetun Krogh <70878501+frodehk@users.noreply.github.com>
2024-08-08 13:46:20 +0200

    chore: update generic charts to not allow 0 efficiency
    ECALC-977

--- src/libecalc/dto/models/chart.py
+++ src/libecalc/dto/models/chart.py
@@ -95,11 +95,11 @@ class VariableSpeedChart(EcalcBaseModel):
 
 class GenericChartFromDesignPoint(EcalcBaseModel):
     typ: Literal[ChartType.GENERIC_FROM_DESIGN_POINT] = ChartType.GENERIC_FROM_DESIGN_POINT
-    polytropic_efficiency_fraction: float = Field(..., ge=0, le=1)
+    polytropic_efficiency_fraction: float = Field(..., gt=0, le=1)
     design_rate_actual_m3_per_hour: float = Field(..., ge=0)
     design_polytropic_head_J_per_kg: float = Field(..., ge=0)
 
 
 class GenericChartFromInput(EcalcBaseModel):
     typ: Literal[ChartType.GENERIC_FROM_INPUT] = ChartType.GENERIC_FROM_INPUT
-    polytropic_efficiency_fraction: float = Field(..., ge=0, le=1)
+    polytropic_efficiency_fraction: float = Field(..., gt=0, le=1)
