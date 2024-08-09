a38bb28f33ba55dcf2048b5490d67de989d9debe
Frode Helgetun Krogh <70878501+frodehk@users.noreply.github.com>
2024-08-09 09:53:15 +0200

    chore!: do not allow 0 efficiency in input chart (#565)
    * chore: do not allow 0 efficiency in input chart
    
    ECALC-977

--- src/libecalc/dto/models/chart.py
+++ src/libecalc/dto/models/chart.py
@@ -13,7 +13,7 @@ class ChartCurve(EcalcBaseModel):
     speed_rpm: float = Field(..., ge=0)
     rate_actual_m3_hour: List[Annotated[float, Field(ge=0)]]
     polytropic_head_joule_per_kg: List[Annotated[float, Field(ge=0)]]
-    efficiency_fraction: List[Annotated[float, Field(ge=0, le=1)]]
+    efficiency_fraction: List[Annotated[float, Field(gt=0, le=1)]]
 
     @model_validator(mode="after")
     def validate_equal_lengths_and_sort(self) -> Self:
@@ -95,11 +95,11 @@ class VariableSpeedChart(EcalcBaseModel):
 
 class GenericChartFromDesignPoint(EcalcBaseModel):
     typ: Literal[ChartType.GENERIC_FROM_DESIGN_POINT] = ChartType.GENERIC_FROM_DESIGN_POINT
-    polytropic_efficiency_fraction: float = Field(..., ge=0, le=1)
+    polytropic_efficiency_fraction: float = Field(..., gt=0, le=1)
     design_rate_actual_m3_per_hour: float = Field(..., ge=0)
     design_polytropic_head_J_per_kg: float = Field(..., ge=0)
 
 
 class GenericChartFromInput(EcalcBaseModel):
     typ: Literal[ChartType.GENERIC_FROM_INPUT] = ChartType.GENERIC_FROM_INPUT
-    polytropic_efficiency_fraction: float = Field(..., ge=0, le=1)
+    polytropic_efficiency_fraction: float = Field(..., gt=0, le=1)
