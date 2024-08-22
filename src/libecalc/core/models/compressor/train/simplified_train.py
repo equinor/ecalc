636ec12ffaaffe1d4844953a8c5b8932933f07b0
Jostein Solaas <josas@equinor.com>
2024-08-22 11:22:18 +0200

    chore: ruff manual fixes

--- src/libecalc/core/models/compressor/train/simplified_train.py
+++ src/libecalc/core/models/compressor/train/simplified_train.py
@@ -173,9 +173,10 @@ class CompressorTrainSimplified(CompressorTrainModel):
                 efficiency_as_function_of_rate_and_head = stage.compressor_chart.efficiency_as_function_of_rate_and_head
             else:
                 # Static efficiency regardless of rate and head. This happens if Generic chart from input is used.
-                efficiency_as_function_of_rate_and_head = lambda rates, heads: np.full_like(  # noqa
-                    rates, fill_value=stage.polytropic_efficiency, dtype=float
-                )
+                def efficiency_as_function_of_rate_and_head(rates, heads):
+                    return np.full_like(
+                        rates, fill_value=stage.polytropic_efficiency, dtype=float
+                    )
 
             polytropic_enthalpy_change_joule_per_kg, polytropic_efficiency = calculate_enthalpy_change_head_iteration(
                 inlet_streams=inlet_streams,
