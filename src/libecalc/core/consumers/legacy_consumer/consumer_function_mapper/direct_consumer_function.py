520ac1b6193828bc806c7be3c3437a4f62d33292
Jostein Solaas <josas@equinor.com>
2024-09-23 20:43:59 +0200

    refactor: don't use dto in DirectExpressionConsumerFunction

--- src/libecalc/core/consumers/legacy_consumer/consumer_function_mapper/direct_consumer_function.py
+++ src/libecalc/core/consumers/legacy_consumer/consumer_function_mapper/direct_consumer_function.py
@@ -5,4 +5,11 @@ from libecalc.dto import DirectConsumerFunction
 
 
 def create_direct_consumer_function(model_dto: DirectConsumerFunction) -> DirectExpressionConsumerFunction:
-    return DirectExpressionConsumerFunction(model_dto)
+    return DirectExpressionConsumerFunction(
+        energy_usage_type=model_dto.energy_usage_type,
+        condition=model_dto.condition,
+        fuel_rate=model_dto.fuel_rate,
+        load=model_dto.load,
+        power_loss_factor=model_dto.power_loss_factor,
+        consumption_rate_type=model_dto.consumption_rate_type,
+    )
