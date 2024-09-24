54ba13534bbd04a7a484b04ebb2a4fd0769d88c6
Kjetil Brakstad <kjbrak@equinor.com>
2024-09-24 11:33:50 +0200

    chore: move poetry env installation from post-create.sh to dockerfile

--- .devcontainer/python/post-create.sh
+++ .devcontainer/python/post-create.sh
@@ -3,10 +3,6 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Install Poetry dependencies
-echo "Installing Poetry dependencies..."
-poetry install
-
 # Install Pre-commit hooks
 echo "Installing Pre-commit hooks..."
 pre-commit install
