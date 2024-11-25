f65231d60615246c4d43ed7ac74b14bb3195cfbc
Frode Helgetun Krogh <70878501+frodehk@users.noreply.github.com>
2024-11-25 18:35:50 +0100

    refactor: remove redundant direct resource service
    ECALC-1813

--- src/libecalc/presentation/yaml/resource_service.py
+++ src/libecalc/presentation/yaml/resource_service.py
@@ -2,7 +2,6 @@ import abc
 from typing import Protocol
 
 from libecalc.presentation.yaml.resource import Resource
-from libecalc.presentation.yaml.yaml_entities import MemoryResource
 from libecalc.presentation.yaml.yaml_models.yaml_model import YamlValidator
 
 
@@ -13,11 +12,3 @@ class ResourceService(Protocol):
 
     @abc.abstractmethod
     def get_resources(self, configuration: YamlValidator) -> dict[str, Resource]: ...
-
-
-class DirectResourceService(ResourceService):
-    def __init__(self, resources):
-        self.resources = resources
-
-    def get_resources(self, configuration: YamlValidator) -> dict[str, MemoryResource]:
-        return self.resources
