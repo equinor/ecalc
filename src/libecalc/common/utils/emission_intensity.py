86b2a6afcbac6d2c2ced7cad8da502248719e6bd
Frode Helgetun Krogh <70878501+frodehk@users.noreply.github.com>
2025-02-14 11:32:27 +0100

    refactor: remove unused method
    equinor/ecalc-internal#303

--- src/libecalc/common/utils/emission_intensity.py
+++ src/libecalc/common/utils/emission_intensity.py
@@ -1,6 +1,3 @@
-from datetime import datetime
-
-from libecalc.common.time_utils import Period, Periods
 from libecalc.common.units import Unit
 from libecalc.common.utils.rates import (
     TimeSeriesIntensity,
@@ -19,27 +16,11 @@ class EmissionIntensity:
         self.hydrocarbon_export_cumulative = hydrocarbon_export_cumulative
         self.unit = unit
         self.periods = emission_cumulative.periods
-        self.yearly_periods = self._create_yearly_periods()
-        self.time_vector = self.periods.all_dates
-        self.start_years = [period.start.year for period in self.periods]
-
-    def _create_yearly_periods(self) -> Periods:
-        yearly_periods = []
-        added_periods = set()
-
-        for period in self.periods:
-            for year in range(period.start.year, period.end.year + 1):
-                start_date = datetime(year, 1, 1)
-                end_date = datetime(year + 1, 1, 1)
-                period_tuple = (start_date, end_date)
-
-                if period_tuple not in added_periods:
-                    yearly_periods.append(Period(start=start_date, end=end_date))
-                    added_periods.add(period_tuple)
-
-        return Periods(yearly_periods)
 
     def calculate_intensity_periods(self):
+        """
+        Calculate the emission intensity for each period over the entire data range.
+        """
         emission_volumes = self.emission_cumulative.to_volumes()
         hydrocarbon_export_volumes = self.hydrocarbon_export_cumulative.to_volumes()
 
@@ -54,7 +35,9 @@ class EmissionIntensity:
         )
 
     def calculate_intensity_cumulative(self) -> TimeSeriesIntensity:
-        """Write description here"""
+        """
+        Calculate the cumulative emission intensity over the entire data period.
+        """
         intensity = self.emission_cumulative / self.hydrocarbon_export_cumulative
 
         return TimeSeriesIntensity(
