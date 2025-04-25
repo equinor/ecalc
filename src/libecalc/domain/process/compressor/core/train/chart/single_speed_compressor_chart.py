ea42b0472b6f5c83990682de0e80dcb463b74c71
olelod <opl@equinor.com>
2025-04-25 12:43:39 +0200

    chore: make sure enthalpy calculations for simplified train works with both scalar and array input
    chore: make sure single point compressor chart results are floats

--- src/libecalc/domain/process/compressor/core/train/chart/single_speed_compressor_chart.py
+++ src/libecalc/domain/process/compressor/core/train/chart/single_speed_compressor_chart.py
@@ -78,8 +78,8 @@ class SingleSpeedCompressorChart(SingleSpeedChart):
         polytropic_efficiency = self.efficiency_as_function_of_rate(rate_corrected_to_minimum_flow)
 
         return CompressorChartHeadEfficiencyResultSinglePoint(
-            polytropic_head=polytropic_head,
-            polytropic_efficiency=polytropic_efficiency,
+            polytropic_head=float(polytropic_head),
+            polytropic_efficiency=float(polytropic_efficiency),
             chart_area_flag=chart_area_flag,
             is_valid=point_is_valid,
         )
