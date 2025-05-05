1a0c9d63398431057d63717e96a7d57c2c0cb70a
Frode Helgetun Krogh <70878501+frodehk@users.noreply.github.com>
2025-05-05 20:18:09 +0200

    test: use builders in test stream conditions

--- tests/libecalc/presentation/yaml/yaml_types/test_yaml_stream_conditions.py
+++ tests/libecalc/presentation/yaml/yaml_types/test_yaml_stream_conditions.py
@@ -1,60 +1,64 @@
 import pytest
 
-
-from libecalc.presentation.yaml.yaml_types.yaml_stream_conditions import YamlEmissionRate, YamlOilVolumeRate
+from libecalc.testing.yaml_builder import YamlEmissionRateBuilder, YamlOilVolumeRateBuilder
 
 
 def test_yaml_emission_rate_valid_condition():
-    rate = YamlEmissionRate(value="10", condition="x > 5")
+    rate = YamlEmissionRateBuilder().with_value(10).with_condition("x > 5").validate()
+
     assert rate.condition == "x > 5"
     assert rate.conditions is None
 
 
 def test_yaml_emission_rate_valid_conditions():
-    rate = YamlEmissionRate(value="10", conditions=["x > 5", "y < 10"])
+    rate = YamlEmissionRateBuilder().with_value(10).with_conditions(["x > 5", "y < 10"]).validate()
+
     assert rate.condition is None
     assert rate.conditions == ["x > 5", "y < 10"]
 
 
 def test_yaml_emission_rate_defaults():
-    rate = YamlEmissionRate(value="10")
+    rate = YamlEmissionRateBuilder().with_value(10)
     assert rate.condition is None
     assert rate.conditions is None
 
 
 def test_yaml_emission_rate_empty_conditions():
     with pytest.raises(ValueError, match="CONDITIONS cannot be an empty list."):
-        YamlEmissionRate(value="10", conditions=[])
+        YamlEmissionRateBuilder().with_value(10).with_conditions([]).validate()
 
 
 def test_yaml_emission_rate_condition_and_conditions_mutual_exclusion():
     with pytest.raises(ValueError, match="Either CONDITION or CONDITIONS should be specified, not both."):
-        YamlEmissionRate(value="10", condition="x > 5", conditions=["x > 5", "y < 10"])
+        YamlEmissionRateBuilder().with_value(10).with_condition("x > 5").with_conditions(["x > 5", "y < 10"]).validate()
 
 
 def test_yaml_oil_volume_rate_valid_condition():
-    rate = YamlOilVolumeRate(value="10", condition="x > 5")
+    rate = YamlOilVolumeRateBuilder().with_value(10).with_condition("x > 5").validate()
     assert rate.condition == "x > 5"
     assert rate.conditions is None
 
 
 def test_yaml_oil_volume_rate_valid_conditions():
-    rate = YamlOilVolumeRate(value="10", conditions=["x > 5", "y < 10"])
+    rate = YamlOilVolumeRateBuilder().with_value(10).with_conditions(["x > 5", "y < 10"]).validate()
+
     assert rate.condition is None
     assert rate.conditions == ["x > 5", "y < 10"]
 
 
 def test_yaml_oil_volume_rate_defaults():
-    rate = YamlOilVolumeRate(value="10")
+    rate = YamlOilVolumeRateBuilder().with_value(10).validate()
     assert rate.condition is None
     assert rate.conditions is None
 
 
 def test_yaml_oil_volume_rate_empty_conditions():
     with pytest.raises(ValueError, match="CONDITIONS cannot be an empty list."):
-        YamlOilVolumeRate(value="10", conditions=[])
+        YamlOilVolumeRateBuilder().with_value(10).with_conditions([]).validate()
 
 
 def test_yaml_oil_volume_rate_condition_and_conditions_mutual_exclusion():
     with pytest.raises(ValueError, match="Either CONDITION or CONDITIONS should be specified, not both."):
-        YamlOilVolumeRate(value="10", condition="x > 5", conditions=["x > 5", "y < 10"])
+        YamlOilVolumeRateBuilder().with_value(10).with_condition("x > 5").with_conditions(
+            ["x > 5", "y < 10"]
+        ).validate()
