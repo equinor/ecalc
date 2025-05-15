ae718821b4968ca6a9f12deb5cb7188f04183577
Aleksander Karlsson <AMKA@equinor.com>
2025-05-15 13:02:46 +0200

    fix: validate reading of resources (#881)
    * fix: reading of resources
    
    * docs: update changelog
    
    Refs:
    https://github.com/equinor/ecalc-internal/issues/740
    
    Refs:
    https://github.com/equinor/ecalc-internal/issues/755

--- src/libecalc/presentation/yaml/domain/time_series_resource.py
+++ src/libecalc/presentation/yaml/domain/time_series_resource.py
@@ -1,5 +1,4 @@
 import logging
-import re
 from collections.abc import Iterable
 from datetime import datetime
 from math import isnan
@@ -7,11 +6,9 @@ from typing import Self
 
 import numpy as np
 import pandas as pd
-from pandas.errors import ParserError
 
 from libecalc.common.errors.exceptions import (
     InvalidColumnException,
-    InvalidHeaderException,
     InvalidResourceException,
     NoColumnsException,
 )
@@ -38,13 +35,9 @@ class DuplicateDatesException(InvalidTimeSeriesResourceException):
         super().__init__(f"The time series resource contains duplicate dates: {','.join(map(str, duplicates))}")
 
 
-def _is_header_valid(header: str) -> bool:
-    return bool(re.match(r"^[A-Za-z][A-Za-z0-9_.,\-\s#+:\/]*$", header))
-
-
 class TimeSeriesResource(Resource):
     """
-    A time series resource containing time series
+    A time series resource containing time series.
     """
 
     def __init__(self, resource: Resource):
@@ -53,13 +46,6 @@ class TimeSeriesResource(Resource):
 
         if len(headers) == 0:
             raise InvalidResourceException("Invalid resource", "Resource must at least have one column")
-
-        for header in headers:
-            if not _is_header_valid(header):
-                raise InvalidHeaderException(
-                    "The time series resource header contains illegal characters. "
-                    "Allowed characters are: ^[A-Za-z][A-Za-z0-9_.,\\-\\s#+:\\/]*$"
-                )
 
         if EcalcYamlKeywords.date in headers:
             # Find the column named "DATE" and use that as time vector
@@ -78,7 +64,7 @@ class TimeSeriesResource(Resource):
                     "Time vector contains values that are not int or str, possibly caused by an extra comma."
                 )
             self._time_vector = self._parse_time_vector(time_vector)
-        except (ParserError, ValueError) as e:
+        except ValueError as e:
             # pandas.to_datetime might raise these two exceptions.
             # See https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.to_datetime.html
             raise InvalidTimeSeriesResourceException(f"Could not parse time vector: {str(e)}") from e
--- src/libecalc/presentation/yaml/yaml_entities.py
+++ src/libecalc/presentation/yaml/yaml_entities.py
@@ -1,8 +1,20 @@
+import math
+import re
 from dataclasses import dataclass
 from enum import Enum
-from typing import TextIO, get_args
+from io import StringIO
+from pathlib import Path
+from typing import Self, TextIO, get_args
 
-from libecalc.common.errors.exceptions import ColumnNotFoundException, HeaderNotFoundException
+import pandas as pd
+
+from libecalc.common.errors.exceptions import (
+    ColumnNotFoundException,
+    HeaderNotFoundException,
+    InvalidColumnException,
+    InvalidHeaderException,
+    InvalidResourceException,
+)
 from libecalc.domain.process.compressor.dto.model_types import CompressorModelTypes
 from libecalc.domain.process.dto.base import EnergyModel
 from libecalc.domain.process.dto.tabulated import TabulatedData
@@ -15,8 +27,11 @@ from libecalc.presentation.yaml.domain.reference_service import InvalidReference
 
 @dataclass
 class MemoryResource(Resource):
-    """
-    Resource object where the data is already read and parsed.
+    """Resource object where the data is already read and parsed.
+
+    Attributes:
+        headers: Header names.
+        data: Column contents.
     """
 
     headers: list[str]
@@ -26,6 +41,14 @@ class MemoryResource(Resource):
         return self.headers
 
     def get_column(self, header: str) -> list[float | int | str]:
+        """Get data in the specified column.
+
+        Args:
+            header: Header name to get column data for.
+
+        Returns:
+            Column data.
+        """
         try:
             header_index = self.headers.index(header)
             return self.data[header_index]
@@ -34,6 +57,111 @@ class MemoryResource(Resource):
         except IndexError as e:
             # Should validate that header and columns are of equal length, but that is currently done elsewhere.
             raise ColumnNotFoundException(header=header) from e
+
+    def validate(self, allow_nans: bool) -> Self:
+        self._validate_headers(self.headers)
+        if not allow_nans:
+            self._validate_not_nan(self.data, self.headers)
+        return self
+
+    @classmethod
+    def from_string(cls, csv_data: str, allow_nans: bool) -> Self:
+        """Read csv strings with settings:
+            comment=#, float_precision="round_trip", skipinitialspace=True, and thousands=" ".
+
+        Args:
+            csv_data: Comma separated data.
+            allow_nans: Whether to fail validation on nan values.
+
+        Returns:
+            MemoryResource.
+        """
+        try:
+            df_resource = pd.read_csv(
+                StringIO(csv_data), comment="#", float_precision="round_trip", skipinitialspace=True, thousands=" "
+            )
+        except pd.errors.ParserError as e:
+            msg = str(e)
+            if "Expected" in msg:
+                msg = "Expected" + msg.split("Expected", 1)[1]
+            raise InvalidResourceException(title="Invalid CSV data", message=msg) from e
+        except ValueError as e:
+            raise InvalidResourceException(title="Invalid resource", message=str(e)) from e
+
+        headers = df_resource.columns.str.strip().tolist()
+        cls._validate_headers(headers)
+
+        if not allow_nans:
+            tmp_data: list[list[float | int | str]] = df_resource.T.values.tolist()
+            cls._validate_not_nan(tmp_data, headers)
+
+        data: list[list[float | int | str]]
+        if df_resource.empty:
+            # Empty column data is fine, but it must retain the shape of the headers.
+            data = [[] * len(headers)]
+        else:
+            data = df_resource.T.values.tolist()
+
+        return MemoryResource(headers=headers, data=data)
+
+    @classmethod
+    def from_path(cls, path: Path | str, allow_nans: bool) -> Self:
+        """Read csv strings with settings equals to `from_string`.
+
+        Args:
+            path: Path to csv file.
+            allow_nans: Whether to fail validation on nan values.
+
+        Returns:
+            MemoryResource.
+        """
+        with open(path) as f:
+            return MemoryResource.from_string(f.read(), allow_nans=allow_nans)
+
+    @staticmethod
+    def _validate_headers(headers: list[str]) -> None:
+        """Ensure headers only contains allowed characters, and no unnamed columns exist.
+
+        Args:
+            headers: The headers to validate.
+
+        Raises:
+            InvalidHeaderException: If headers do not follow naming conventions, or are unnamed.
+        """
+        for header in headers:
+            if not re.match(r"^[A-Za-z][A-Za-z0-9_.,\-\s#+:/]*$", header):
+                raise InvalidHeaderException(
+                    "Each header value must start with a letter in the english alphabet (a-zA-Z). "
+                    "Header may only contain letters, spaces, numbers, or any of the following characters "
+                    "[ _ - # + : . , /]."
+                )
+            if re.match(r"^Unnamed: \d+$", header):
+                raise InvalidHeaderException(message="One or more headers are missing in resource")
+
+    @staticmethod
+    def _validate_not_nan(rows: list[list[float | int | str]], headers: list[str] | None = None) -> None:
+        """Find the first value containing NaN and raise an error message with row index and optional column name.
+
+        Args:
+            rows: The contents of the columns.
+            headers: Optional, if set it is used to give better error messages. Defaults to None.
+
+        Raises:
+            InvalidColumnException: If there are NaNs in the data.
+        """
+        err_header = ""
+        for i, row in enumerate(rows):
+            for index_col, item in enumerate(row):
+                if isinstance(item, float) and math.isnan(item):
+                    if headers is not None:
+                        err_header = headers[index_col]
+                    raise InvalidColumnException(
+                        header=err_header,
+                        message=(
+                            "CSV file contains invalid data all headers must be associated with a valid column value."
+                        ),
+                        row=i,
+                    )
 
 
 @dataclass
