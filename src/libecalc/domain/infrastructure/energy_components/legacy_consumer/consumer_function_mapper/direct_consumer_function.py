e1b51841592d921efd326986b915f54c1daf05f6
Kjetil Brakstad <kjbrak@equinor.com>
2025-05-22 12:30:01 +0200

    fix: fixed mypy errors in legacy_consumer

--- src/libecalc/domain/infrastructure/energy_components/legacy_consumer/consumer_function_mapper/direct_consumer_function.py
+++ src/libecalc/domain/infrastructure/energy_components/legacy_consumer/consumer_function_mapper/direct_consumer_function.py
@@ -7,9 +7,9 @@ from libecalc.domain.process.dto import DirectConsumerFunction
 def create_direct_consumer_function(model_dto: DirectConsumerFunction) -> DirectExpressionConsumerFunction:
     return DirectExpressionConsumerFunction(
         energy_usage_type=model_dto.energy_usage_type,
-        condition=model_dto.condition,
-        fuel_rate=model_dto.fuel_rate,
-        load=model_dto.load,
-        power_loss_factor=model_dto.power_loss_factor,
+        condition=model_dto.condition,  # type: ignore[arg-type]
+        fuel_rate=model_dto.fuel_rate,  # type: ignore[arg-type]
+        load=model_dto.load,  # type: ignore[arg-type]
+        power_loss_factor=model_dto.power_loss_factor,  # type: ignore[arg-type]
         consumption_rate_type=model_dto.consumption_rate_type,
     )
