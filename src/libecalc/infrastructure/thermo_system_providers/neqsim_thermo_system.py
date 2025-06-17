b2d534a59134a871a98c585b5053fb498384d3c1
Kjetil Brakstad <kjbrak@equinor.com>
2025-06-17 15:34:17 +0200

    chore: add explicit protocol inheritance for NeqSimThermoSystem

--- src/libecalc/infrastructure/thermo_system_providers/neqsim_thermo_system.py
+++ src/libecalc/infrastructure/thermo_system_providers/neqsim_thermo_system.py
@@ -6,9 +6,10 @@ from ecalc_neqsim_wrapper.thermo import NeqsimFluid
 from libecalc.domain.process.value_objects.fluid_stream.conditions import ProcessConditions
 from libecalc.domain.process.value_objects.fluid_stream.eos_model import EoSModel
 from libecalc.domain.process.value_objects.fluid_stream.fluid_composition import FluidComposition
+from libecalc.domain.process.value_objects.fluid_stream.thermo_system import ThermoSystemInterface
 
 
-class NeqSimThermoSystem:
+class NeqSimThermoSystem(ThermoSystemInterface):
     """
     Implementation of ThermoSystemInterface for NeqSim.
     Maintains a single 'live' NeqsimFluid object for the composition & EoS
