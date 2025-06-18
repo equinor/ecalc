aa6aba40508237ecfbc6a45e66d5191718938cff
Kjetil Brakstad <kjbrak@equinor.com>
2025-06-18 09:53:35 +0200

    test: make a choke valve invalid pressure exception test

--- tests/libecalc/domain/process/process_units/choke_valve/test_choke_valve.py
+++ tests/libecalc/domain/process/process_units/choke_valve/test_choke_valve.py
@@ -2,6 +2,7 @@ import pytest
 
 from libecalc.domain.common.entity_id import SimpleEntityID
 from libecalc.domain.process.entities.process_units.choke_valve import ChokeValve
+from libecalc.domain.process.entities.process_units.choke_valve.exceptions import InvalidPressureDropException
 from libecalc.domain.process.entities.process_units.port_names import SingleIO
 
 
@@ -14,3 +15,18 @@ def test_single_choke_valve_pressure_drop(fluid_stream_mock):
 
     outlet_stream = valve.outlet_stream
     assert outlet_stream.pressure_bara == pytest.approx(fluid_stream_mock.pressure_bara - 5.0)
+
+
+def test_choke_valve_invalid_pressure_drop(fluid_stream_mock):
+    """Test that InvalidPressureDropException is raised when inlet pressure is lower than delta_p."""
+    # fluid_stream_mock has 20.0 bara pressure, create valve with 100 bar pressure drop
+    valve = ChokeValve(SimpleEntityID("tv-1"), delta_p_bar=100.0)
+
+    # Should raise InvalidPressureDropException when trying to connect
+    with pytest.raises(InvalidPressureDropException) as exc_info:
+        valve.connect_inlet_port(SingleIO.INLET, fluid_stream_mock)
+
+    # Verify the exception contains relevant information
+    assert "20.00 bara" in str(exc_info.value)  # inlet pressure
+    assert "100.00 bar" in str(exc_info.value)  # pressure drop
+    assert "negative pressure not allowed" in str(exc_info.value)
