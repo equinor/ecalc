4a6ee70ca0750262702ae8fe8b4f935578a8ca8b
Jostein Solaas <josas@equinor.com>
2025-06-19 13:01:19 +0200

    chore: test JPype, jneqsim

--- src/ecalc_neqsim_wrapper/java_service.py
+++ src/ecalc_neqsim_wrapper/java_service.py
@@ -1,9 +1,7 @@
 import logging
-import os
-from os import path
 from typing import Optional
 
-from py4j.java_gateway import JavaGateway
+from jneqsim import neqsim
 
 _logger = logging.getLogger(__name__)
 
@@ -12,38 +10,9 @@ _logger = logging.getLogger(__name__)
 _neqsim_service: Optional["NeqsimService"] = None
 
 
-_local_os_name = os.name
-_colon = ":"
-if _local_os_name == "nt":
-    _colon = ";"
-
-
-def _create_classpath(jars):
-    """Create path to NeqSim .jar file"""
-    resources_dir = path.dirname(__file__) + "/lib"
-    return _colon.join([path.join(resources_dir, jar) for jar in jars])
-
-
-def _start_server(maximum_memory: str = "4G") -> JavaGateway:
-    """
-    Start JVM for NeqSim Wrapper
-    Returns: (int, Popen) port, process
-
-    """
-    jars = ["NeqSim.jar"]
-    classpath = _create_classpath(jars)
-
-    logging.getLogger("py4j").setLevel(logging.ERROR)
-    return JavaGateway.launch_gateway(classpath=classpath, die_on_exit=False, javaopts=[f"-Xmx{maximum_memory}"])
-
-
 class NeqsimService:
     def __init__(self, maximum_memory: str = "4G"):
         global _neqsim_service
-        self._gateway = _start_server(maximum_memory=maximum_memory)
-        _logger.info(
-            f"Started neqsim process with PID '{self._gateway.java_process.pid}' on port '{self._gateway.gateway_parameters.port}'"
-        )
         _neqsim_service = self
 
     def __enter__(self):
@@ -53,20 +22,10 @@ class NeqsimService:
         self.shutdown()
 
     def get_neqsim_module(self):
-        return self._gateway.jvm.neqsim
+        return neqsim
 
     def shutdown(self):
-        global _neqsim_service
-        _logger.info(
-            f"Killing neqsim process with PID '{self._gateway.java_process.pid}' on port '{self._gateway.gateway_parameters.port}'"
-        )
-        # Shutdown gateway, connections ++
-        try:
-            self._gateway.shutdown()
-        except Exception:
-            _logger.exception("Java gateway close failed")
-        finally:
-            _neqsim_service = None
+        pass
 
 
 def get_neqsim_service():
--- src/ecalc_neqsim_wrapper/thermo.py
+++ src/ecalc_neqsim_wrapper/thermo.py
@@ -7,7 +7,6 @@ from functools import cached_property, lru_cache
 from pathlib import Path
 from typing import Protocol, assert_never
 
-from py4j.protocol import Py4JJavaError
 from pydantic import BaseModel
 
 from ecalc_neqsim_wrapper.components import COMPONENTS
@@ -165,19 +164,6 @@ class NeqsimFluid:
             pressure_bara=pressure_bara,
             mixing_rule=mixing_rule,
         )
-
-        # Since we are caching the java objects, they will contain the connection info to the java process (py4j).
-        # That connection info might be outdated; clear the cache if that is the case.
-        if thermodynamic_system._gateway_client.port != get_neqsim_service().get_neqsim_module()._gateway_client.port:
-            cls._init_thermo_system.cache_clear()
-            thermodynamic_system = cls._init_thermo_system(
-                components=components,
-                molar_fraction=molar_fractions,
-                eos_model_type=eos_model,
-                temperature_kelvin=temperature_kelvin,
-                pressure_bara=pressure_bara,
-                mixing_rule=mixing_rule,
-            )
 
         return cls(thermodynamic_system=thermodynamic_system, use_gerg=use_gerg)
 
@@ -421,7 +407,7 @@ def get_GERG2008_properties(thermodynamic_system: ThermodynamicSystem):
     """
     try:  # Using getPhase(0) instead of getPhase("gas") to handle dense fluids correctly
         gas_phase = thermodynamic_system.getPhase(0)
-    except Py4JJavaError as e:
+    except Exception as e:
         msg = "Could not get gas phase. Make sure the fluid is specified correctly."
         raise NeqsimPhaseError(msg) from e
     gerg_properties = gas_phase.getProperties_GERG2008()
