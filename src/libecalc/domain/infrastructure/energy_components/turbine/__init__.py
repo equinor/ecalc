ca53fc5a8b8d26c3262f41219371a36b5034c459
Frode Helgetun Krogh <70878501+frodehk@users.noreply.github.com>
2025-06-20 14:04:24 +0200

    refactor/remove-turbine-dto

---
+++ src/libecalc/domain/infrastructure/energy_components/turbine/__init__.py
@@ -0,0 +1,1 @@
+from .turbine import Turbine
--- src/libecalc/domain/process/dto/__init__.py
+++ src/libecalc/domain/process/dto/__init__.py
@@ -1,4 +1,3 @@
 from .base import ConsumerFunction, EnergyModel
 from .sampled import EnergyModelSampled
 from .tabulated import TabulatedData
-from .turbine import Turbine
