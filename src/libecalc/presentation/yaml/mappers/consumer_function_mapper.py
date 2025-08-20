4a34e410da2a1a43e13d787c673de02ddc4da5c0
Jostein Solaas <josas@equinor.com>
2025-08-20 10:49:22 +0200

    fix: filter consumers with no model for period
    When END is specified and a consumer with late start exists, we might
    have a consumer without a model. This makes sure the consumer is
    filtered out.

--- src/libecalc/presentation/yaml/mappers/consumer_function_mapper.py
+++ src/libecalc/presentation/yaml/mappers/consumer_function_mapper.py
@@ -636,7 +636,7 @@ class ConsumerFunctionMapper:
     def from_yaml_to_dto(
         self,
         consumes: ConsumptionType,
-    ) -> TemporalModel[ConsumerFunction]:
+    ) -> TemporalModel[ConsumerFunction] | None:
         temporal_dict: dict[Period, ConsumerFunction] = {}
         for period, model in self._time_adjusted_model.items():
             try:
@@ -675,4 +675,8 @@ class ConsumerFunctionMapper:
                     period=period,
                     model=model,
                 ) from e
+
+        if len(temporal_dict) == 0:
+            return None
+
         return TemporalModel(temporal_dict)
