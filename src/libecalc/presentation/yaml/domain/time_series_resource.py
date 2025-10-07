fix: oopsie with calculation of no. lines with time

src/libecalc/presentation/yaml/domain/time_series_resource.py
@@ -168,7 +168,7 @@ class TimeSeriesResource(Resource):
 
         if (has_time := check_dates.str.fullmatch(r".*(\s(\d{2}:){2}\d{2})$")).any():
             if not (has_time.all() or not has_time.any()):
-                most_with_time = len(has_time) > len(check_dates)
+                most_with_time = has_time.sum() > (len(check_dates) // 2)
                 locations = np.flatnonzero(~has_time) + 1 if most_with_time else np.flatnonzero(has_time) + 1
                 message += (
                     f"Mostly dates {'with' if most_with_time else 'without'} time present, "