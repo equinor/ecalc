051675d57353249fd261fee6d403b5797963bc33
Jostein Solaas <josas@equinor.com>
2025-12-04 09:10:23 +0100

    fix: issue with expression validation in genset and venting emitter
    Use YamlExpressionType to validate expression when validating for run.
    
    Refs:
    https://github.com/equinor/ecalc-internal/issues/1050

--- src/libecalc/presentation/yaml/yaml_types/components/yaml_generator_set.py
+++ src/libecalc/presentation/yaml/yaml_types/components/yaml_generator_set.py
@@ -2,11 +2,11 @@ from pydantic import ConfigDict, Field, model_validator
 
 from libecalc.dto.types import ConsumerUserDefinedCategoryType
 from libecalc.dto.utils.validators import ComponentNameStr
-from libecalc.expression.expression import ExpressionType
 from libecalc.presentation.yaml.ltp_validation import validate_generator_set_power_from_shore
 from libecalc.presentation.yaml.yaml_types import YamlBase
 from libecalc.presentation.yaml.yaml_types.components.legacy.yaml_electricity_consumer import YamlElectricityConsumer
 from libecalc.presentation.yaml.yaml_types.components.yaml_category_field import CategoryField
+from libecalc.presentation.yaml.yaml_types.components.yaml_expression_type import YamlExpressionType
 from libecalc.presentation.yaml.yaml_types.models.model_reference_validation import GeneratorSetModelReference
 from libecalc.presentation.yaml.yaml_types.yaml_temporal_model import YamlTemporalModel
 
@@ -31,10 +31,10 @@ class YamlGeneratorSet(YamlBase):
         description="Specifies the correlation between the electric power delivered and the fuel burned by a "
         "generator set.\n\n$ECALC_DOCS_KEYWORDS_URL/ELECTRICITY2FUEL",
     )
-    cable_loss: ExpressionType | None = Field(
+    cable_loss: YamlExpressionType | None = Field(
         None, title="CABLE_LOSS", description="Cable loss from shore, fraction of from shore consumption"
     )
-    max_usage_from_shore: ExpressionType | None = Field(
+    max_usage_from_shore: YamlExpressionType | None = Field(
         None,
         title="MAX_USAGE_FROM_SHORE",
         description="The peak load/effect that is expected for one hour, per year (MW)",
