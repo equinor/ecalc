f71600ac98502088f042dc91a45e64ec3acef4a5
Frode Helgetun Krogh <70878501+frodehk@users.noreply.github.com>
2025-12-05 09:54:08 +0100

    refactor: rename evaluation input args for clarity

--- src/libecalc/domain/process/evaluation_input.py
+++ src/libecalc/domain/process/evaluation_input.py
@@ -19,30 +19,30 @@ class CompressorEvaluationInput:
     """
     Encapsulates all input data required to configure and apply evaluation parameters to a compressor model.
 
-    Attributes:
-        rate_expression (TimeSeriesFlowRate | list[TimeSeriesFlowRate]): the flow rate expression(s).
+    Args:
+        rate (TimeSeriesFlowRate | list[TimeSeriesFlowRate]): the flow rate expression(s).
         fluid_factory (FluidFactoryInterface | list[FluidFactoryInterface] | None): the fluid factory instance describing compositional properties for the process fluid.
         power_loss_factor (TimeSeriesPowerLossFactor | None): expression for power loss factor.
-        suction_pressure_expression (TimeSeriesPressure | None): expression for the inlet (suction) pressure.
-        discharge_pressure_expression (TimeSeriesPressure | None): expression for the outlet (discharge) pressure.
-        intermediate_pressure_expression (TimeSeriesPressure | None): expression for intermediate pressure.
+        suction_pressure (TimeSeriesPressure | None): expression for the inlet (suction) pressure.
+        discharge_pressure (TimeSeriesPressure | None): expression for the outlet (discharge) pressure.
+        intermediate_pressure (TimeSeriesPressure | None): expression for intermediate pressure.
     """
 
     def __init__(
         self,
-        rate_expression: TimeSeriesFlowRate | list[TimeSeriesFlowRate],
+        rate: TimeSeriesFlowRate | list[TimeSeriesFlowRate],
         fluid_factory: FluidFactoryInterface | list[FluidFactoryInterface],
-        suction_pressure_expression: TimeSeriesPressure,
-        discharge_pressure_expression: TimeSeriesPressure,
+        suction_pressure: TimeSeriesPressure,
+        discharge_pressure: TimeSeriesPressure,
         power_loss_factor: TimeSeriesPowerLossFactor | None = None,
-        intermediate_pressure_expression: TimeSeriesPressure | None = None,
+        intermediate_pressure: TimeSeriesPressure | None = None,
     ):
-        self._rate_expression = rate_expression
+        self._rate = rate
         self._fluid_factory = fluid_factory
         self._power_loss_factor = power_loss_factor
-        self._suction_pressure_expression = suction_pressure_expression
-        self._discharge_pressure_expression = discharge_pressure_expression
-        self._intermediate_pressure_expression = intermediate_pressure_expression
+        self._suction_pressure = suction_pressure
+        self._discharge_pressure = discharge_pressure
+        self._intermediate_pressure = intermediate_pressure
 
     @property
     def power_loss_factor(self) -> TimeSeriesPowerLossFactor | None:
@@ -50,12 +50,12 @@ class CompressorEvaluationInput:
 
     @property
     def periods(self) -> Periods:
-        if isinstance(self._rate_expression, list):
-            return self._rate_expression[0].get_periods()
-        return self._rate_expression.get_periods()
+        if isinstance(self._rate, list):
+            return self._rate[0].get_periods()
+        return self._rate.get_periods()
 
     def apply_to_model(self, compressor_model: CompressorTrainModel | CompressorWithTurbineModel):
-        rate_expr = self._rate_expression if isinstance(self._rate_expression, list) else [self._rate_expression]
+        rate_expr = self._rate if isinstance(self._rate, list) else [self._rate]
 
         model_to_check = (
             compressor_model.compressor_model
@@ -70,12 +70,12 @@ class CompressorEvaluationInput:
             stream_day_rate = np.array([rate.get_stream_day_values() for rate in rate_expr], dtype=np.float64)
 
         intermediate_pressure = (
-            np.asarray(self._intermediate_pressure_expression.get_values(), dtype=np.float64)
-            if self._intermediate_pressure_expression is not None
+            np.asarray(self._intermediate_pressure.get_values(), dtype=np.float64)
+            if self._intermediate_pressure is not None
             else None
         )
-        suction_pressure = np.asarray(self._suction_pressure_expression.get_values(), dtype=np.float64)
-        discharge_pressure = np.asarray(self._discharge_pressure_expression.get_values(), dtype=np.float64)
+        suction_pressure = np.asarray(self._suction_pressure.get_values(), dtype=np.float64)
+        discharge_pressure = np.asarray(self._discharge_pressure.get_values(), dtype=np.float64)
 
         compressor_model.set_evaluation_input(
             rate=stream_day_rate,
@@ -90,24 +90,24 @@ class CompressorSampledEvaluationInput:
     """
     Encapsulates all input data required to configure and apply evaluation parameters to a sampled compressor model.
 
-    Attributes:
-        rate_expression (TimeSeriesFlowRate | list[TimeSeriesFlowRate]): the flow rate expression(s).
+    Args:
+        rate (TimeSeriesFlowRate | list[TimeSeriesFlowRate]): the flow rate expression(s).
         power_loss_factor (TimeSeriesPowerLossFactor | None): expression for power loss factor.
-        suction_pressure_expression (TimeSeriesPressure | None): expression for the inlet (suction) pressure.
-        discharge_pressure_expression (TimeSeriesPressure | None): expression for the outlet (discharge) pressure.
+        suction_pressure (TimeSeriesPressure | None): expression for the inlet (suction) pressure.
+        discharge_pressure (TimeSeriesPressure | None): expression for the outlet (discharge) pressure.
     """
 
     def __init__(
         self,
-        rate_expression: TimeSeriesFlowRate | list[TimeSeriesFlowRate],
+        rate: TimeSeriesFlowRate | list[TimeSeriesFlowRate],
         power_loss_factor: TimeSeriesPowerLossFactor | None = None,
-        suction_pressure_expression: TimeSeriesPressure | None = None,
-        discharge_pressure_expression: TimeSeriesPressure | None = None,
+        suction_pressure: TimeSeriesPressure | None = None,
+        discharge_pressure: TimeSeriesPressure | None = None,
     ):
-        self._rate_expression = rate_expression
+        self._rate = rate
         self._power_loss_factor = power_loss_factor
-        self._suction_pressure_expression = suction_pressure_expression
-        self._discharge_pressure_expression = discharge_pressure_expression
+        self._suction_pressure = suction_pressure
+        self._discharge_pressure = discharge_pressure
 
     @property
     def power_loss_factor(self) -> TimeSeriesPowerLossFactor | None:
@@ -115,24 +115,24 @@ class CompressorSampledEvaluationInput:
 
     @property
     def periods(self) -> Periods:
-        if isinstance(self._rate_expression, list):
-            return self._rate_expression[0].get_periods()
-        return self._rate_expression.get_periods()
+        if isinstance(self._rate, list):
+            return self._rate[0].get_periods()
+        return self._rate.get_periods()
 
     def apply_to_model(self, compressor_model: CompressorModelSampled | CompressorWithTurbineModel):
-        rate_expr = [self._rate_expression]
+        rate_expr = [self._rate]
 
         assert len(rate_expr) == 1
         stream_day_rate = np.asarray(rate_expr[0].get_stream_day_values(), dtype=np.float64)
 
         suction_pressure = (
-            np.asarray(self._suction_pressure_expression.get_values(), dtype=np.float64)
-            if self._suction_pressure_expression is not None
+            np.asarray(self._suction_pressure.get_values(), dtype=np.float64)
+            if self._suction_pressure is not None
             else None
         )
         discharge_pressure = (
-            np.asarray(self._discharge_pressure_expression.get_values(), dtype=np.float64)
-            if self._discharge_pressure_expression is not None
+            np.asarray(self._discharge_pressure.get_values(), dtype=np.float64)
+            if self._discharge_pressure is not None
             else None
         )
 
