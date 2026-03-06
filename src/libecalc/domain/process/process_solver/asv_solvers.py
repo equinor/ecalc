d742f241d84d8c80f09959659dc622472acb35e2
Frode Helgetun Krogh <70878501+frodehk@users.noreply.github.com>
2026-03-06 15:32:38 +0100

    chore: update docstring

--- src/libecalc/domain/process/process_solver/asv_solvers.py
+++ src/libecalc/domain/process/process_solver/asv_solvers.py
@@ -28,7 +28,7 @@ class ASVSolver:
 
     Attributes:
         _shaft (Shaft): The shaft associated with the compressors.
-        _compressors (list[libecalc.domain.process.process_system.compressor_stage_process_unit.CompressorStageProcessUnit]): List of compressor stage process units.
+        _compressors (list[CompressorStageProcessUnit): List of compressor stage process units.
         _fluid_service (FluidService): The fluid service used in the process.
         _root_finding_strategy (ScipyRootFindingStrategy): Strategy for root finding.
     """
