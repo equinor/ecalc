722da92e6b417e0c43073554f0b78aafaf4128c1
Frode Helgetun Krogh <70878501+frodehk@users.noreply.github.com>
2026-04-16 16:26:56 +0200

    chore: remove init electric drive train

--- src/libecalc/domain/energy/drive_train/electric_drive_train.py
+++ src/libecalc/domain/energy/drive_train/electric_drive_train.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 from libecalc.domain.energy.drive_train.drive_train import DriveTrain, DriveTrainResult
-from libecalc.domain.energy.rotating_equipment.rotating_equipment import RotatingEquipment
 
 
 @dataclass
@@ -19,13 +18,6 @@ class ElectricDriveTrain(DriveTrain):
     accounting for mechanical losses. Does not know where the electricity comes from —
     that's the responsibility of a PowerSupply.
     """
-
-    def __init__(
-        self,
-        rotating_equipment: list[RotatingEquipment],
-        mechanical_efficiency: float = 1.0,
-    ):
-        super().__init__(rotating_equipment=rotating_equipment, mechanical_efficiency=mechanical_efficiency)
 
     def evaluate(self) -> ElectricDriveTrainResult:
         shaft_power = self.get_total_shaft_power_demand_mw()
