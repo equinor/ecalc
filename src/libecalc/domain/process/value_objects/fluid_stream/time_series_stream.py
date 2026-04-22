c95e74ed69e32415d1b376b6b06ec47646af68e0
Thomas Johansen <tj098895@equinor.com>
2026-04-22 21:50:37 +0200

    chore: separate ephemeral inlet stream from domain stream

--- src/libecalc/domain/process/value_objects/fluid_stream/time_series_stream.py
+++ src/libecalc/domain/process/value_objects/fluid_stream/time_series_stream.py
@@ -1,13 +1,16 @@
 from dataclasses import dataclass
 
-from libecalc.domain.process.value_objects.fluid_stream import FluidModel
+from libecalc.domain.process.value_objects.fluid_stream import EoSModel
 from libecalc.presentation.yaml.domain.expression_time_series_flow_rate import ExpressionTimeSeriesFlowRate
 from libecalc.presentation.yaml.domain.time_series_expression import TimeSeriesExpression
 
 
 @dataclass(frozen=True)
 class TimeSeriesStream:
-    fluid_model: FluidModel
+    eos_model: EoSModel  # TODO: Fixed, but is a "strategy" and should be provided as a strategy. Should we support different eos models for different problems/trains?
+    composition: (
+        TimeSeriesExpression  # currently considered timeseries property of a stream, not related to "domain events"
+    )
     pressure_bara: TimeSeriesExpression
     temperature_kelvin: TimeSeriesExpression
     standard_rate_m3_per_day: ExpressionTimeSeriesFlowRate
