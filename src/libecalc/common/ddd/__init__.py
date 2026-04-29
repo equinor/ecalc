9df364788e3e4a820e297895822ba6ecdd5e542c
Thomas Johansen <tj098895@equinor.com>
2026-04-29 21:21:58 +0200

    chore: docstring first

--- src/libecalc/common/ddd/__init__.py
+++ src/libecalc/common/ddd/__init__.py
@@ -1,6 +1,3 @@
-from libecalc.common.ddd.entity import Entity
-from libecalc.common.ddd.value_object import value_object
-
 """
 DDD Tactical Building Blocks
 
@@ -9,5 +6,8 @@ the things right, we reuse the building blocks here in code,
 instead of redoing everything all the time, in slightly
 different ways, and possibly introducing bugs.
 """
+
+from libecalc.common.ddd.entity import Entity
+from libecalc.common.ddd.value_object import value_object
 
 __all__ = ["Entity", "value_object"]
