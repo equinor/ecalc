679d85d68c752746fa987ab96654aca414b22c59
Thomas Johansen <tj098895@equinor.com>
2026-05-04 17:22:47 +0200

    chore: initial arch tests

--- tests/architectural_tests/debug_pyarch.py
+++ tests/architectural_tests/debug_pyarch.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from pytestarch import get_evaluable_architecture
 
 if __name__ == "__main__":
-    root_dir = Path(__file__).parent.parent.parent.parent / "libecalc" / "src"
-    src_dir = root_dir.parent / "src"
+    root_dir = Path(__file__).parent.parent.parent.parent / "libecalc" / "src" / "libecalc"
+    src_dir = root_dir.parent / "libecalc"
     arch = get_evaluable_architecture(str(root_dir), str(src_dir))
     arch.visualize()
--- tests/architectural_tests/test_enforce_hexagonal_architecture.py
+++ tests/architectural_tests/test_enforce_hexagonal_architecture.py
@@ -1,14 +1,39 @@
 from pytestarch import Rule
 
 
-def test_domain_does_not_import_infrastructure(libecalc_architecture):
+def test_common_to_not_import_process(libecalc_architecture):
     rule = (
         Rule()
         .modules_that()
-        .are_sub_modules_of("libecalc.process")
+        .are_sub_modules_of("libecalc.common")
         .should_not()
         .import_modules_that()
-        .are_sub_modules_of("libecalc.common")
+        .are_sub_modules_of("libecalc.process")
     )
 
     rule.assert_applies(libecalc_architecture)
+
+
+def test_process_to_import_process_or_common_only(libecalc_architecture):
+    # TODO: libecalc.domain due to train utils
+    # TODO: libecalc.domain due to charts
+    for forbidden_process_dep in [
+        "libecalc.application",
+        "libecalc.core",
+        "libecalc.dto",
+        "libecalc.ecalc_model",
+        "libecalc.examples",
+        "libecalc.expression",
+        "libecalc.fixtures",
+        "libecalc.infrastructure",
+        "libecalc.presentation",
+    ]:
+        rule = (
+            Rule()
+            .modules_that()
+            .are_sub_modules_of("libecalc.process")
+            .should_not()
+            .import_modules_that()
+            .are_sub_modules_of(forbidden_process_dep)
+        )
+        rule.assert_applies(libecalc_architecture)
