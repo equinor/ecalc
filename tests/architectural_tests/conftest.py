0b4c874bc1275f44f9ea9337fc1913e658c1cfc2
Thomas Johansen <tj098895@equinor.com>
2026-05-04 23:10:46 +0200

    test: fix tests

--- tests/architectural_tests/conftest.py
+++ tests/architectural_tests/conftest.py
@@ -4,17 +4,17 @@ import pytest
 from pytestarch import EvaluableArchitecture, get_evaluable_architecture
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def root_dir() -> Path:
     root_dir = Path(__file__).parent.parent.parent.parent / "libecalc" / "src" / "libecalc"
     return root_dir
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def src_dir(root_dir) -> Path:
     return root_dir.parent / "libecalc"
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def libecalc_architecture(root_dir, src_dir: Path) -> EvaluableArchitecture:
     return get_evaluable_architecture(str(root_dir), str(src_dir))
