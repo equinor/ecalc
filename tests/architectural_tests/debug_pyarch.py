4b5d419eecd980debe868b32b3077b7d7ab3ede3
Thomas Johansen <tj098895@equinor.com>
2026-05-05 10:26:45 +0200

    chore: viz module deps

--- tests/architectural_tests/debug_pyarch.py
+++ tests/architectural_tests/debug_pyarch.py
@@ -1,9 +1,19 @@
+"""
+Very simple way of visualizing the architecture/modules in libecalc.
+Might be useful.
+Currently set to top level modules only.
+"""
+
 from pathlib import Path
 
+from matplotlib import pyplot as plt
 from pytestarch import get_evaluable_architecture
 
 if __name__ == "__main__":
-    root_dir = Path(__file__).parent.parent.parent.parent / "libecalc" / "src" / "libecalc"
-    src_dir = root_dir.parent / "libecalc"
-    arch = get_evaluable_architecture(str(root_dir), str(src_dir))
+    root_dir = Path(__file__).parent.parent.parent
+    root_dir = root_dir / "src" / "libecalc"
+    arch = get_evaluable_architecture(
+        str(root_dir), str(root_dir), level_limit=1, exclusions=("*__pycache__*", "*__init__.py*")
+    )
     arch.visualize()
+    plt.show()
