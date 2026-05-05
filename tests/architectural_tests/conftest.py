ba994fa7a6afb24273b0c0eac19978d966fd8560
Jostein Solaas <33114722+jsolaas@users.noreply.github.com>
2026-05-05 09:49:49 +0200

    chore: don't assume repo name in arch tests

--- tests/architectural_tests/conftest.py
+++ tests/architectural_tests/conftest.py
@@ -6,15 +6,17 @@ from pytestarch import EvaluableArchitecture, get_evaluable_architecture
 
 @pytest.fixture(scope="function")
 def root_dir() -> Path:
-    root_dir = Path(__file__).parent.parent.parent.parent / "libecalc" / "src" / "libecalc"
-    return root_dir
+    root_dir = Path(__file__).parent.parent.parent
+    return root_dir / "src" / "libecalc"
 
 
 @pytest.fixture(scope="function")
 def src_dir(root_dir) -> Path:
-    return root_dir.parent / "libecalc"
+    return root_dir
 
 
 @pytest.fixture(scope="function")
 def libecalc_architecture(root_dir, src_dir: Path) -> EvaluableArchitecture:
-    return get_evaluable_architecture(str(root_dir), str(src_dir))
+    arch = get_evaluable_architecture(str(root_dir), str(src_dir))
+    print(arch.modules)
+    return arch
