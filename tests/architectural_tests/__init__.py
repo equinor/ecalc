e980fb3c3d0e17db557a0ee07f9f98fa5436cf78
Thomas Johansen <tj098895@equinor.com>
2026-05-05 10:36:05 +0200

    chore: add tests for ecalc_model

--- tests/architectural_tests/__init__.py
+++ tests/architectural_tests/__init__.py
@@ -2,4 +2,9 @@
 Tests for new code/architecture only, we want to
 mainly focus on imports first, ie. following
 hexagonal/onion/clean architecture rules
+
+Currently we have only a few modules that we actually
+have control of and can enforce the rules. We extend
+tests of more modules, and remove "exceptions" when
+we can.
 """
--- tests/architectural_tests/test_enforce_hexagonal_architecture.py
+++ tests/architectural_tests/test_enforce_hexagonal_architecture.py
@@ -34,3 +34,21 @@ def test_process_to_import_process_or_common_only(libecalc_architecture):
         .are_named(allowed_process_dependencies)
     )
     rule.assert_applies(libecalc_architecture)
+
+
+@pytest.mark.arch
+def test_ecalc_model_to_import_ecalc_model_or_common_only(libecalc_architecture):
+    allowed_process_dependencies = [
+        "libecalc.common",
+        "libecalc.process",  # Should ecalc_model "module" be allowed to pick stuff from core domains (process, energy, emission)?
+        "libecalc.presentation",  # TODO: timeseries stuff, to be moved to ecalc_model?
+    ]
+    rule = (
+        Rule()
+        .modules_that()
+        .are_named("libecalc.ecalc_model")
+        .should_only()
+        .import_modules_that()
+        .are_named(allowed_process_dependencies)
+    )
+    rule.assert_applies(libecalc_architecture)
