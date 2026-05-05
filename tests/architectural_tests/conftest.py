d22e9ced2be6223ec1a16e1e8b0ecbee9b53b177
Jostein Solaas <33114722+jsolaas@users.noreply.github.com>
2026-05-05 10:02:07 +0200

    chore: session scope, remove print

--- tests/architectural_tests/conftest.py
+++ tests/architectural_tests/conftest.py
@@ -4,19 +4,18 @@ import pytest
 from pytestarch import EvaluableArchitecture, get_evaluable_architecture
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def root_dir() -> Path:
     root_dir = Path(__file__).parent.parent.parent
     return root_dir / "src" / "libecalc"
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def src_dir(root_dir) -> Path:
     return root_dir
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def libecalc_architecture(root_dir, src_dir: Path) -> EvaluableArchitecture:
     arch = get_evaluable_architecture(str(root_dir), str(src_dir))
-    print(arch.modules)
     return arch
