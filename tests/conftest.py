00440443336efdf351a797e670e1b1e52b423006
Frode Helgetun Krogh <70878501+frodehk@users.noreply.github.com>
2026-06-01 16:01:59 +0200

    chore: revert change

--- tests/conftest.py
+++ tests/conftest.py
@@ -648,7 +648,6 @@ def patch_uuid():
 def process_simulation_mapper(
     fluid_service,
     expression_evaluator_factory,
-    with_neqsim_service,
 ):
     """
     Minimal ProcessSimulationMapper for testing.
