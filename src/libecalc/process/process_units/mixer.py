ac7c230d0cbf5fc174d419d22511e03a5923337f
Kjetil Brakstad <kjbrak@equinor.com>
2026-06-11 12:07:05 +0200

    feat: isenthalpic stream mixing (#1622)
    Replace mass-weighted-temperature mixing with adiabatic (isenthalpic)
    mixing: conserve total enthalpy and PH-flash to the outlet pressure for
    the correct outlet temperature and phase.
    
    Rename SimplifiedStreamMixer to StreamMixer and move it to
    process/fluid_stream. Update the mixing test with an enthalpy-balance
    check and assert the outlet pressure is the lowest inlet pressure.

--- src/libecalc/process/process_units/mixer.py
+++ src/libecalc/process/process_units/mixer.py
@@ -2,10 +2,8 @@ from typing import Final
 
 from libecalc.process.fluid_stream.fluid_service import FluidService
 from libecalc.process.fluid_stream.fluid_stream import FluidStream
+from libecalc.process.fluid_stream.stream_mixer import StreamMixer
 from libecalc.process.process_pipeline.process_unit import ProcessUnit, ProcessUnitId
-from libecalc.process.process_units.simplified_stream_mixer import (
-    SimplifiedStreamMixer,
-)
 
 
 class Mixer(ProcessUnit):
@@ -15,9 +13,13 @@ class Mixer(ProcessUnit):
     a sidestream injection point in an interstage manifold.
     """
 
-    def __init__(self, fluid_service: FluidService, process_unit_id: ProcessUnitId | None = None):
+    def __init__(
+        self,
+        fluid_service: FluidService,
+        process_unit_id: ProcessUnitId | None = None,
+    ):
         self._id: Final[ProcessUnitId] = process_unit_id or ProcessUnit._create_id()
-        self._mixer = SimplifiedStreamMixer(fluid_service)
+        self._mixer = StreamMixer(fluid_service)
         self._external_stream: FluidStream | None = None
 
     def get_id(self) -> ProcessUnitId:
